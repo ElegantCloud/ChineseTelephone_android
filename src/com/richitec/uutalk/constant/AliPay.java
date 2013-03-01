35a257b1d20f6eaa6495c5fcac674e2dbdbb0ed6
starkingwx <starkingwx@gmail.com>
2013-03-01 18:56:06 +0800

    change 安中通 to 环宇通

--- src/com/richitec/uutalk/constant/AliPay.java
+++ src/com/richitec/uutalk/constant/AliPay.java
@@ -1,7 +1,7 @@
 package com.richitec.uutalk.constant;
 
 public class AliPay {
-	public final static String aliPaySubject = "安中通账户充值";
-	public final static String aliPayBody = "安中通账户充值";
+	public final static String aliPaySubject = "环宇通账户充值";
+	public final static String aliPayBody = "环宇通账户充值";
 	public final static String aliPayType = "alipay";
 }
