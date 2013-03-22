72dde3525e0a03257b105ac84367120f0f7b53dd
starkingwx <starkingwx@gmail.com>
2013-03-22 18:58:00 +0800

    add background image for account setting, register and get password
    jump to main page after registered

--- src/com/richitec/chinesetelephone/account/AccountDirectRegisterActivity.java
+++ src/com/richitec/chinesetelephone/account/AccountDirectRegisterActivity.java
@@ -2,6 +2,7 @@ package com.richitec.chinesetelephone.account;
 
 import java.util.HashMap;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
@@ -9,7 +10,10 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnFocusChangeListener;
@@ -21,20 +25,28 @@ import android.widget.Toast;
 
 import com.richitec.chinesetelephone.R;
 import com.richitec.chinesetelephone.constant.SystemConstants;
+import com.richitec.chinesetelephone.constant.TelUser;
+import com.richitec.chinesetelephone.tab7tabcontent.ChineseTelephoneTabActivity;
 import com.richitec.chinesetelephone.utils.AppDataSaveRestoreUtil;
 import com.richitec.chinesetelephone.utils.CountryCodeManager;
+import com.richitec.commontoolkit.user.User;
+import com.richitec.commontoolkit.user.UserBean;
+import com.richitec.commontoolkit.user.UserManager;
+import com.richitec.commontoolkit.utils.DataStorageUtils;
 import com.richitec.commontoolkit.utils.HttpUtils;
 import com.richitec.commontoolkit.utils.HttpUtils.HttpRequestType;
 import com.richitec.commontoolkit.utils.HttpUtils.HttpResponseResult;
 import com.richitec.commontoolkit.utils.HttpUtils.OnHttpRequestListener;
 import com.richitec.commontoolkit.utils.HttpUtils.PostRequestFormat;
 import com.richitec.commontoolkit.utils.MyToast;
+import com.richitec.commontoolkit.utils.StringUtils;
 
 public class AccountDirectRegisterActivity extends Activity {
 	private AlertDialog chooseCountryDialog;
 	private int lastSelect = 0;
 	CountryCodeManager countryCodeManager;
 	private ProgressDialog progressDlg;
+	private String passwordText;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -103,6 +115,9 @@ public class AccountDirectRegisterActivity extends Activity {
 		String phoneNumber = phoneNumberET.getText().toString().trim();
 		String pwd = pwdET.getText().toString().trim();
 		String pwd1 = pwd1ET.getText().toString().trim();
+
+		passwordText = pwd;
+
 		if (phoneNumber == null || phoneNumber.equals("")) {
 			MyToast.show(this, R.string.number_cannot_be_null,
 					Toast.LENGTH_SHORT);
@@ -138,6 +153,20 @@ public class AccountDirectRegisterActivity extends Activity {
 		params.put("countryCode", countryCode);
 		params.put("phoneNumber", phoneNumber);
 		params.put("password", pwd);
+
+		params.put("brand", Build.BRAND);
+		params.put("model", Build.MODEL);
+		params.put("release", Build.VERSION.RELEASE);
+		params.put("sdk", Build.VERSION.SDK);
+
+		DisplayMetrics dm = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+		int nowWidth = dm.widthPixels; // 当前分辨率 宽度
+		int nowHeigth = dm.heightPixels; // 当前分辨率高度
+		params.put("width", Integer.toString(nowWidth));
+		params.put("height", Integer.toString(nowHeigth));
+
 		HttpUtils.postRequest(getString(R.string.server_url)
 				+ getString(R.string.direct_reg_url),
 				PostRequestFormat.URLENCODED, params, null,
@@ -155,20 +184,69 @@ public class AccountDirectRegisterActivity extends Activity {
 						responseResult.getResponseText());
 				String result = data.getString("result");
 				if (result.equals("0")) {
-					// register ok, jump to login view
-					new AlertDialog.Builder(AccountDirectRegisterActivity.this)
-							.setTitle(R.string.alert_title)
-							.setMessage(R.string.register_ok)
-							.setPositiveButton(R.string.ok,
-									new DialogInterface.OnClickListener() {
+					// login automatically
+					try {
+						String userName = data.getString("username");
+						String countryCode = data.getString("countrycode");
+						String userKey = data.getString("userkey");
+						String vosphone = data.getString("vosphone");
+						String vosphone_psw = data.getString("vosphone_pwd");
+						String bindPhone = data.getString("bindphone");
+						String bindPhoneCountryCode = data
+								.getString("bindphone_country_code");
+						String email = null;
+						try {
+							email = data.getString("email");
+						} catch (JSONException e) {
+						}
+						Double regGivenMoney = data
+								.getDouble("reg_given_money");
 
-										@Override
-										public void onClick(
-												DialogInterface dialog,
-												int which) {
-											finish();
-										}
-									}).show();
+						UserBean telUser = UserManager.getInstance().getUser();
+						telUser.setName(userName);
+						telUser.setValue(TelUser.countryCode.name(),
+								countryCode);
+						telUser.setRememberPwd(true);
+						telUser.setValue(TelUser.dialCountryCode.name(),
+								countryCode);
+						telUser.setPassword(StringUtils.md5(passwordText));
+						telUser.setUserKey(userKey);
+						telUser.setValue(TelUser.vosphone.name(), vosphone);
+						telUser.setValue(TelUser.vosphone_pwd.name(),
+								vosphone_psw);
+						telUser.setValue(TelUser.bindphone.name(), bindPhone);
+						telUser.setValue(TelUser.bindphone_country_code.name(),
+								bindPhoneCountryCode);
+						saveUserAccount();
+
+						Intent intent = new Intent(
+								AccountDirectRegisterActivity.this,
+								ChineseTelephoneTabActivity.class);
+						intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+								| Intent.FLAG_ACTIVITY_CLEAR_TOP);
+						intent.putExtra("email", email);
+						intent.putExtra("reg_given_money", regGivenMoney);
+						startActivity(intent);
+						finish();
+
+					} catch (JSONException e) {
+						e.printStackTrace();
+						new AlertDialog.Builder(
+								AccountDirectRegisterActivity.this)
+								.setTitle(R.string.alert_title)
+								.setMessage(R.string.register_ok)
+								.setPositiveButton(R.string.ok,
+										new DialogInterface.OnClickListener() {
+
+											@Override
+											public void onClick(
+													DialogInterface dialog,
+													int which) {
+												finish();
+											}
+										}).show();
+					}
+
 				} else if (result.equals("1")) {
 					MyToast.show(AccountDirectRegisterActivity.this,
 							R.string.number_cannot_be_null, Toast.LENGTH_SHORT);
@@ -189,6 +267,33 @@ public class AccountDirectRegisterActivity extends Activity {
 				e.printStackTrace();
 				MyToast.show(AccountDirectRegisterActivity.this,
 						R.string.error_in_regsiter, Toast.LENGTH_SHORT);
+			}
+		}
+
+		private void saveUserAccount() {
+			Log.d(SystemConstants.TAG, "save user account");
+			UserBean user = UserManager.getInstance().getUser();
+			Log.d(SystemConstants.TAG, "user: " + user.toString());
+			DataStorageUtils.putObject(User.username.name(), user.getName());
+			DataStorageUtils.putObject(TelUser.countryCode.name(),
+					user.getValue(TelUser.countryCode.name()));
+			DataStorageUtils.putObject(TelUser.dialCountryCode.name(),
+					user.getValue(TelUser.dialCountryCode.name()));
+
+			DataStorageUtils.putObject(TelUser.vosphone.name(),
+					user.getValue(TelUser.vosphone.name()));
+			DataStorageUtils.putObject(TelUser.vosphone_pwd.name(),
+					user.getValue(TelUser.vosphone_pwd.name()));
+			DataStorageUtils.putObject(User.userkey.name(), user.getUserKey());
+			DataStorageUtils.putObject(TelUser.bindphone.name(),
+					user.getValue(TelUser.bindphone.name()));
+			DataStorageUtils.putObject(TelUser.bindphone_country_code.name(),
+					user.getValue(TelUser.bindphone_country_code.name()));
+			if (user.isRememberPwd()) {
+				DataStorageUtils.putObject(User.password.name(),
+						user.getPassword());
+			} else {
+				DataStorageUtils.putObject(User.password.name(), "");
 			}
 		}
 
