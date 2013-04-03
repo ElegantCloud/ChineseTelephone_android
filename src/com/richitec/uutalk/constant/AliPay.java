2aeaa4c5b0463302f466ae93e1a40d9e76d626db
starkingwx <starkingwx@gmail.com>
2013-04-03 12:08:41 +0800

    rename to uutalk

--- src/com/richitec/uutalk/constant/AliPay.java
+++
@@ -1,7 +0,0 @@
-package com.richitec.uutalk.constant;
-
-public class AliPay {
-	public final static String aliPaySubject = "环宇通账户充值";
-	public final static String aliPayBody = "环宇通账户充值";
-	public final static String aliPayType = "alipay";
-}
